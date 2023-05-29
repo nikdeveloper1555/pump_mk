2c7bb4893962a09cf969f64a5e807278c003d55c
Nikita <nik.developer1555@gmail.com>
2023-05-29 14:05:37 +0200

    Update conditions

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ TOKEN = '6039018327:AAFcQln4yB2r4mCwR0tOchGuV7jOTjnlHw8'
 bot = Bot(token=TOKEN)
 chat_id_pump = -1001557765670
 chat_id_dump = -1001824440975
-perc_deltas = [4, 4, 10, 10, 15]  # persantage
-time_frames = [60, 300, 600, 1800, 3600]  # time frames
+perc_deltas = [0.7, 1.5, 3, 10, 15]  # persantage
+time_frames = [15, 60, 300, 1800, 3600]  # time frames
 ADMIN = 824679469
 
